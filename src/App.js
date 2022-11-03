b53fa83f8535012c60b7a36c12550788e7247fdd
burakeldn <96900698+burakeldn@users.noreply.github.com>
2022-11-03 11:29:56 +0300

    |

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import "./style.css"
+import React, {useState} from "react"
+
 
 function App() {
+
+  const [minVal, setMinVal] = useState(0)
+  const [maxVal, setMaxVal] = useState(0)
+  const [randomNum, setRandomNum] = useState(0)
+
+  const giveRandomNumber = ()=>{
+    setRandomNum(Math.floor(Math.random()*(maxVal-minVal+1)+minVal))
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +30,29 @@ function App() {
           Learn React
         </a>
       </header>
+    <div className="main">
+      <div className="container">
+        <div className="randomNumber">
+          <p>Random Number: <span>{randomNum}</span></p>
+        </div>
+        <div className="numberContainer">
+          <div>
+            <p>Min: </p>
+            <input 
+              type="number" 
+              value={minVal}
+              onChange={e => setMinVal(+e.target.value)}/>
+          </div>
+          <div>
+            <p>Max: </p>
+            <input 
+              type="number"
+              value={maxVal}
+              onChange={e=> setMaxVal(+e.target.value)}/>
+          </div>
+        </div>
+        <button onClick={giveRandomNumber}>Get Random Number</button>
+      </div>
     </div>
   );
 }
