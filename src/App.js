5f5ef3603f6f72f8ca3f94a53e80f06efa049f7a
Burak ELDEN <96900698+burakeldn@users.noreply.github.com>
2022-11-03 13:50:33 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-import logo from './logo.svg';
-import './App.css';
 import "./style.css"
 import React, {useState} from "react"
 
@@ -15,22 +13,8 @@ function App() {
   }
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    <div className="main">
+   
+     <div className="main">
       <div className="container">
         <div className="randomNumber">
           <p>Random Number: <span>{randomNum}</span></p>
